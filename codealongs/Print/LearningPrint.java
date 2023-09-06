7291a2a13c02376a04e3d67c3a77aab6dbafc76d
stephenbadcock <stephenbadcock@hotmail.com>
2023-09-06 10:04:02 +0200

    My own LearningJava

---
+++ codealongs/Print/LearningPrint.java
@@ -0,0 +1,16 @@
+package codealongs.Print.finished;
+
+public class LearningPrint {
+    public static void main(String[] args) {
+        System.out.println("H");
+        System.out.println("H");
+        System.out.println("\u00E4");
+
+        int cars = 8;
+        System.out.println(cars);
+
+        System.out.print("hello");
+        System.out.print("hello");
+        System.out.printf("hello");
+    }
+}

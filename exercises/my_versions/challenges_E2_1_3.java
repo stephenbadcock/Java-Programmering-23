fcfb83c35efd32bf8b450ce989b52428a7215f7b
stephenbadcock <stephenbadcock@hotmail.com>
2023-09-14 19:21:29 +0200

    2 övningar E2

---
+++ exercises/my_versions/challenges_E2_1_3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class challenges_E2_1_3 {
+    public static void main(String[] args) {
+        double number;
+        String result, size;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Choose a number: ");
+        number = scanner.nextDouble();
+
+        scanner.close();
+
+        if(number > 0) {
+            result = "positive";
+        } else if(number < 0) {
+            result = "negative";
+        } else {
+            result = "zero";
+        }
+
+        System.out.println("Your number is " + result);
+
+        if(number < 1) {
+            System.out.print("Your number is small");
+        } else if(number > 1000000) {
+            System.out.print("Your number is big");
+        }
+    }
+}
---
+++ exercises/my_versions/challenges_E2_2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class challenges_E2_2 {
+    public static void main(String[] args) {
+        int no1, no2, no3, largest = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Choose a number: ");
+        no1 = sc.nextInt();
+
+        System.out.print("Choose another number: ");
+        no2 = sc.nextInt();
+
+        System.out.print("Choose a third number: ");
+        no3 = sc.nextInt();
+        System.out.println();
+
+        sc.close();
+
+        if(no1 >= no2 && no1 >= no3) {
+            largest = no1;
+        } else if(no2 >= no1 && no2 >= no3) {
+            largest = no2;
+        } else if(no3 >= no1 && no3 >= no2) {
+            largest = no3;
+        }
+
+        System.out.println("The largest number is " + largest);
+    }
+}
